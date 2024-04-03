a66289e1e3f21c5ae1ae98b94743f0ed16a9f12d
Josh <jdd2s@login.missouristate.edu>
2024-04-03 11:28:36 -0500

    just before algo start

--- simpager.cpp
+++ simpager.cpp
@@ -1,16 +1,30 @@
 using namespace std;
 
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 
-string page_refs;
-string frames;
-string algorithms;
-
-void printStats();
+vector<int> convertStringToVectorInt(string refs);
+void printPageReference(vector<int> nums);
+bool simulateAlgorithm(int mapped_algorithm, vector<int> page_refs, int frames);
+bool isNumInVector(int num, vector<int> vec);
 
 int main(int argc, char *argv[])
 {
+    vector<int> int_page_refs;
+    string string_of_refs;
+    int frame_count;
+    string algorithms;
+
     string line;
+
+    unordered_map<string, int> algorithmMap = {
+        {"FIFO", 1},
+        {"LRU", 2},
+        {"OPT", 3},
+        {"RAND", 4}};
 
     int input_section{1};
     // Loop through every line in txt file
@@ -21,25 +35,93 @@ int main(int argc, char *argv[])
             continue;
         switch (input_section++)
         {
-        case 1:;
-            page_refs = line;
+        case 1:
+            string_of_refs = line;
+            int_page_refs = convertStringToVectorInt(string_of_refs);
+            printPageReference(int_page_refs);
             break;
         case 2:
-            frames = line;
+            frame_count = stoi(line);
+            cout << "Number of frames: " << frame_count << endl;
             break;
         default:
             if (input_section >= 3)
-                algorithms += line + " ";
+                simulateAlgorithm(algorithmMap[line], int_page_refs, frame_count);
             break;
         }
     }
 
-    printStats();
+    for (auto s : algorithms)
+    {
+        cout << s << ": ";
+    }
 }
 
-void printStats()
+bool isNumInVector(int num, vector<int> vec)
 {
-    cout << page_refs << endl;
-    cout << "Number of frames: " << frames << endl;
-    cout << algorithms << endl;
+    for (int n : vec)
+    {
+        if (n == num)
+            return true;
+    }
+    return false;
+}
+
+// given an ID run an algoirthm on page refs with a frame
+bool simulateAlgorithm(int mapped_algorithm, vector<int> page_refs, int frame_count)
+{
+    // initialize a vect of size 'frame_count' with all values of '-1'
+    vector<int> frames(frame_count, -1);
+
+    for (int page_ref : page_refs)
+    {
+        if (isNumInVector(page_ref, frames))
+        {
+            cout << "Yes!" << endl;
+        }
+    }
+
+    switch (mapped_algorithm)
+    {
+    case 1:
+        cout << "FIFO: ";
+        break;
+    case 2:
+        cout << "\nLRU: ";
+        break;
+    case 3:
+        cout << "\nOPT: ";
+        break;
+    case 4:
+        cout << "\nRAND: ";
+        break;
+    default:
+        return false;
+        break;
+    }
+    return true;
+}
+
+vector<int> convertStringToVectorInt(string refs)
+{
+    vector<int> nums;
+    stringstream ss(refs);
+    int num;
+    // Convert page ref string to vector of ints
+    while (ss >> num)
+    {
+        nums.push_back(num);
+    }
+    return nums;
+}
+
+void printPageReference(vector<int> nums)
+{
+    cout << "\nPage Reference String:" << endl;
+    int max_int_per_line = 20;
+    int int_on_line_count = 1;
+    for (auto num : nums)
+    {
+        ((int_on_line_count++) % max_int_per_line == 0) ? cout << num << endl : cout << num << " ";
+    }
 }
